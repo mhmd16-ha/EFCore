9537c581f5259403876d5d07a1e3e9531ddaf99a
يارب <@DESKTOP-GHSHA8O>
2022-11-05 20:39:46 +0200

    Add project files.

---
+++ EFCore/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore
+{
+    public class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EFCore;Integrated Security=True;");
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
